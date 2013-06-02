4a06cd3623dbaf9e339ec3cd0b8f10feb3cdb254
Alex Espinoza <aespinoza@aspria.net>
2013-06-02 15:24:50 -0400

    Alex/Allen acceptance/unit test checkpoint 1

--- spec/features/apps_spec.rb
+++ spec/features/apps_spec.rb
@@ -2,10 +2,39 @@ require 'spec_helper'
 
 describe "Apps" do
 
+	let(:app_submit){FactoryGirl.build(:app)}
+	let(:valid_submit) do
+		fill_in("Name", :with => app_submit.name)
+		fill_in("Url", :with => app_submit.url)
+		fill_in("Email", :with => app_submit.email)
+		fill_in("Codebase url", :with => app_submit.codebase_url)
+		fill_in("Props", :with => app_submit.props)
+	end
+
   describe "viewing the index" do
   	it "should display a list of submitted apps" do
-  	visit '/apps'
-  	expect(page).to have_content("Test app content")
+  	visit '/apps/new'
+  	valid_submit
+  	click_button "Create App"
+  	expect(page).to have_content("CuddleWithUs")
+  	end
+  end
+
+  describe "submitting a app" do
+  	it "should not be valid if any fields are empty" do
+  		visit '/apps/new'
+  		valid_submit
+  		fill_in("Name", :with => nil)
+  		click_button "Create App"
+  		expect(page).to have_content('error prohibited this app from being saved')
+  	end
+
+  	it "should not work if the email address is invalid" do
+  		visit '/apps/new'
+  		valid_submit
+  		fill_in("Email", :with => "hello(at)launchy.com")
+  		click_button "Create App"
+  		expect(page).to have_content('error prohibited this app from being saved')
   	end
   end
 
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -4,10 +4,36 @@ describe App do
   describe "viewing the index" do
   	it "should display a list of submitted apps" do
   		count = App.all.count
-  		3.times {FactoryGirl.build(:app)}
+  		3.times {FactoryGirl.create(:app)}
   		expect(App.all.count).to eq(count + 3)
   	end
+  end
 
+  describe "submitting a app" do
+  	it "will not be valid if name field is empty" do
+  		app = FactoryGirl.build(:app, name: nil)
+  		expect(app).to_not be_valid
+		end
+
+		it "will not be valid if url field is empty" do
+			app = FactoryGirl.build(:app, url: nil)
+  		expect(app).to_not be_valid
+		end
+
+		it "will not be valid if email field is empty" do
+			app = FactoryGirl.build(:app, email: nil)
+  		expect(app).to_not be_valid
+		end
+
+		it "will not be valid if codebase url field is empty" do
+			app = FactoryGirl.build(:app, codebase_url: nil)
+  		expect(app).to_not be_valid
+		end
+
+		it "will not work if the email address is invalid" do
+			app = FactoryGirl.build(:app, email: "test(at)launchy.com")
+			expect(app).to_not be_valid
+		end
   end
 
 end
