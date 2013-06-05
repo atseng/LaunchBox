b5b8d8b551e8b52097dfa9a1081d2f92a0da78bc
Alex Espinoza <aespinoza@aspria.net>
2013-06-05 11:45:10 -0400

    Comments redirects to app#show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,7 +47,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to [@app, @comment], notice: 'Comment was successfully created.' }
+        format.html { redirect_to @app, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
