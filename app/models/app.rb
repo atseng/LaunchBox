c1e8f46c91445c1f1b4eff74f6964821bf91c258
Alex Espinoza <aespinoza@aspria.net>
2013-06-06 21:33:32 -0400

    App submit page styled

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,7 +1,7 @@
 class App < ActiveRecord::Base
-  attr_accessible :codebase_url, :email, :name, :props, :url
+  attr_accessible :codebase_url, :email, :name, :props, :url, :description
   has_many :comments
 
-  validates_presence_of :name, :email, :url, :codebase_url
+  validates_presence_of :name, :email, :url, :codebase_url, :description
   validates_format_of :email, :with => /@/
 end
